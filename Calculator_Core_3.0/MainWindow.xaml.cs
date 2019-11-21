379f16894b34c8defcb67d8073a54f009a8fd604
dahovnikM <dahovnik@gmail.com>
2019-11-21 22:17:23 +0200

    Убраны модификаторы доступа к полям EventHandler.

--- Calculator_Core_3.0/MainWindow.xaml.cs
+++ Calculator_Core_3.0/MainWindow.xaml.cs
@@ -15,133 +15,133 @@ namespace Calculator_Core_3._0
         }
         #region Enents.
 
-        private EventHandler but_0_Click = null;
+        EventHandler but_0_Click = null;
         public event EventHandler But_0_Click
         {
             add { but_0_Click += value; }
             remove { but_0_Click -= value; }
         }
 
-        private EventHandler but_1_Click = null;
+        EventHandler but_1_Click = null;
         public event EventHandler But_1_Click
         {
             add { but_1_Click += value; }
             remove { but_1_Click -= value; }
         }
 
-        private EventHandler but_2_Click = null;
+        EventHandler but_2_Click = null;
         public event EventHandler But_2_Click
         {
             add { but_2_Click += value; }
             remove { but_2_Click -= value; }
         }
 
-        private EventHandler but_3_Click = null;
+        EventHandler but_3_Click = null;
         public event EventHandler But_3_Click
         {
             add { but_3_Click += value; }
             remove { but_3_Click -= value; }
         }
 
-        private EventHandler but_4_Click = null;
+        EventHandler but_4_Click = null;
         public event EventHandler But_4_Click
         {
             add { but_4_Click += value; }
             remove { but_4_Click -= value; }
         }
 
-        private EventHandler but_5_Click = null;
+        EventHandler but_5_Click = null;
         public event EventHandler But_5_Click
         {
             add { but_5_Click += value; }
             remove { but_5_Click -= value; }
         }
 
-        private EventHandler but_6_Click = null;
+        EventHandler but_6_Click = null;
         public event EventHandler But_6_Click
         {
             add { but_6_Click += value; }
             remove { but_6_Click -= value; }
         }
 
-        private EventHandler but_7_Click = null;
+        EventHandler but_7_Click = null;
         public event EventHandler But_7_Click
         {
             add { but_7_Click += value; }
             remove { but_7_Click -= value; }
         }
 
-        private EventHandler but_8_Click = null;
+        EventHandler but_8_Click = null;
         public event EventHandler But_8_Click
         {
             add { but_8_Click += value; }
             remove { but_8_Click -= value; }
         }
 
-        private EventHandler but_9_Click = null;
+        EventHandler but_9_Click = null;
         public event EventHandler But_9_Click
         {
             add { but_9_Click += value; }
             remove { but_9_Click -= value; }
         }
 
-        private EventHandler but_Add_Click = null;
+        EventHandler but_Add_Click = null;
         public event EventHandler But_Add_Click
         {
             add { but_Add_Click += value; }
             remove { but_Add_Click -= value; }
         }
 
-        public EventHandler but_Equals_Click = null;
+        EventHandler but_Equals_Click = null;
         public event EventHandler But_Equals_Click
         {
             add { but_Equals_Click += value; }
             remove { but_Equals_Click -= value; }
         }
 
-        public EventHandler but_Point_Click = null;
+        EventHandler but_Point_Click = null;
         public event EventHandler But_Point_Click
         {
             add { but_Point_Click += value; }
             remove { but_Point_Click -= value; }
         }
 
-        public EventHandler but_Sub_Click = null;
+        EventHandler but_Sub_Click = null;
         public event EventHandler But_Sub_Click
         {
             add { but_Sub_Click += value; }
             remove { but_Sub_Click -= value; }
         }
 
-        public EventHandler but_Mul_Click = null;
+        EventHandler but_Mul_Click = null;
         public event EventHandler But_Mul_Click
         {
             add { but_Mul_Click += value; }
             remove { but_Mul_Click -= value; }
         }
 
-        public EventHandler but_Dev_Click = null;
+        EventHandler but_Dev_Click = null;
         public event EventHandler But_Dev_Click
         {
             add { but_Dev_Click += value; }
             remove { but_Dev_Click -= value; }
         }
 
-        public EventHandler but_Res_Click = null;
+        EventHandler but_Res_Click = null;
         public event EventHandler But_Res_Click
         {
             add { but_Res_Click += value; }
             remove { but_Res_Click -= value; }
         }
 
-        public EventHandler but_ChangeSign_Click = null;
+        EventHandler but_ChangeSign_Click = null;
         public event EventHandler But_ChangeSign_Click
         {
             add { but_ChangeSign_Click += value; }
             remove { but_ChangeSign_Click -= value; }
         }
 
-        public EventHandler but_Corect_Click = null;
+        EventHandler but_Corect_Click = null;
         public event EventHandler But_Corect_Click
         {
             add { but_Corect_Click += value; }
@@ -168,8 +168,6 @@ namespace Calculator_Core_3._0
         private void Button_Division_Click(object sender, RoutedEventArgs e) => but_Dev_Click.Invoke(sender, e);
         private void Button_Reset_Click(object sender, RoutedEventArgs e) => but_Res_Click.Invoke(sender, e);
         private void Button_ChangeSign_Click(object sender, RoutedEventArgs e) => but_ChangeSign_Click.Invoke(sender, e);
-        private void ButtonCorect_Click(object sender, RoutedEventArgs e) => but_Corect_Click.Invoke(sender, e);
+        private void ButtonCorrect_Click(object sender, RoutedEventArgs e) => but_Corect_Click.Invoke(sender, e);
     }
-
 }
-
